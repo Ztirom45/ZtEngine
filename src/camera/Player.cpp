436149d850fc9732dd7a8843b855661735d762ad
Ztirom45 <moritz.busse.mb@web.de>
2023-11-05 13:53:13 +0100

    add Player.cpp and .hpp try 2

---
+++ src/camera/Player.cpp
@@ -0,0 +1,96 @@
+/*
+Player (Camera + Keybord control)  libery from Ztirom's ZtEngine
+
+TIP if you want to know more about the functions take a look into the header
+*/
+
+#include <config.hpp>
+#include <camera/Player.hpp>
+
+Player::Player(glm::vec3 pos,
+	float CameraDistanceToPlayer,float yaw,float pitch,
+	bool *LoopConditionVar)
+	:Camera(pos,CameraDistanceToPlayer,yaw,pitch){
+	this->loop = LoopConditionVar;
+}
+void Player::set_key_state(int position,bool state){
+	if(position>=0&&position<keysSIZE){
+		this->keys[position] = state;
+	}else{
+		printf("Error: key array index out of range (index:%d).\n",position);
+	}
+}
+
+bool Player::get_key_state(int position){
+	if(position>=0&&position<keysSIZE){
+		return keys[position];
+	}else{
+		printf("Error: key array index out of range (index:%d).\n",position);
+		return false;
+	}
+}
+void Player::handle_events(){
+	SDL_Event event;
+	const Uint8 *keyboard_state_array = SDL_GetKeyboardState(NULL);
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+			case SDL_QUIT:
+				*this->loop = false;
+				break;
+			case SDL_KEYDOWN:
+				this->set_key_state(event.key.keysym.sym,true);
+				break;
+			case SDL_KEYUP:
+				this->set_key_state(event.key.keysym.sym,false);
+				break;
+			default:
+				break;
+		}
+	}
+}
+void Player::Move(){//move the player with the inputs in keys
+
+	//transformation
+	if(this->keys[SDLK_w]){
+		this->move_forward(0.1);
+	}
+	if(this->keys[SDLK_s]){
+		this->move_back(0.1);
+	}
+	if(this->keys[SDLK_a]){
+		this->move_left(0.1);
+	}
+	if(this->keys[SDLK_d]){
+		this->move_right(0.1);
+	}
+	if(this->keys[SDLK_SPACE]){
+		this->move_up(0.1);
+	}
+	if(this->keys[SDLK_LSHIFT]){
+		this->move_down(0.1);
+	}
+
+	//rotation
+
+	if(this->keys[SDLK_UP]){
+		this->rotate_pitch(glm::radians(1.0f));
+	}
+	if(this->keys[SDLK_DOWN]){
+		this->rotate_pitch(glm::radians(-1.0f));
+	}
+
+	if(this->keys[SDLK_LEFT]){
+		this->rotate_yaw(glm::radians(1.0f));
+	}
+
+	if(this->keys[SDLK_RIGHT]){
+		this->rotate_yaw(glm::radians(-1.0f));
+	}
+
+};
+
+void Player::update_player(){
+	this->handle_events();
+	this->Move();
+	this->update_camera();
+};
---
+++ src/camera/Player.hpp
@@ -0,0 +1,35 @@
+/*
+Player(Camera + Keybord control) libery from Ztirom's ZtEngine
+
+*/
+
+#pragma once
+
+#include <camera/camera.hpp>
+#define keysSIZE 1073742050
+
+class Player: public Camera{
+	private:
+		bool keys[keysSIZE] = {0};
+	public:
+		//keybordstate array
+
+		// a pointer to the condtion of the main loop,
+		// so that the loop could be quit, if the exit event is triggert
+		bool *loop;
+		Player(glm::vec3,float,float,float,bool*);
+		
+			
+		//set and get keys states funciton to reduce data coruption
+		void set_key_state(int,bool);
+		bool get_key_state(int);
+
+		//translate STL_events into keys(the keyboard state array)
+		void handle_events();
+		
+		//move the player using keys	
+		void Move();
+
+		//update fuction do not use update_camera if you want ot update the whole player
+		void update_player();
+};
